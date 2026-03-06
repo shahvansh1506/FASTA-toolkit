fa88b06b2de2cb57a18befbedbd79ad2cae11644
vansh1506 <shahvansh1506@gmail.com>
2026-03-06 23:25:00 +0530

    Updated README

---
+++ README.md
@@ -0,0 +1,46 @@
+# FASTA Toolkit 🧬
+
+A simple bioinformatics toolkit written in Python for analyzing DNA sequences from FASTA files.
+
+## Features
+
+This toolkit can:
+
+- Read DNA sequences from a FASTA file
+- Calculate sequence length
+- Calculate GC content
+- Generate reverse complement
+- Convert DNA to RNA
+- Handle multiple sequences in a FASTA file
+
+## Requirements
+
+Python 3.x
+
+## Usage
+
+Run the program using:
+
+python main.py sample.fasta
+
+## Example FASTA File
+
+>Sequence_1
+ATGCGTAGCTAGCTAGCTAG
+
+>Sequence_2
+GGCTAGCTAGCGATATCGC
+
+## Example Output
+
+Sequence: Sequence_1  
+Length: 20  
+GC Content: 50%  
+Reverse Complement: CTAGCTAGCTACGCAT  
+RNA: AUGCGUAGCUAGCUAG
+
+---
+
+## Author
+
+Vansh Shah  
--- main.py
+++ main.py
@@ -1,41 +1,52 @@
+def gc_content(sequence):
+    g = sequence.count("G")
+    c = sequence.count("C")
+    return round((g + c) / len(sequence) * 100, 2)
+
+
+def reverse_complement(sequence):
+    complement = {"A":"T", "T":"A", "G":"C", "C":"G"}
+    rev_comp = ""
+
+    for base in reversed(sequence):
+        rev_comp += complement[base]
+
+    return rev_comp
+
+
+def dna_to_rna(sequence):
+    return sequence.replace("T","U")
+
 def read_fasta(file_path):
+    sequences = {}
+    header = ""
+    seq = ""
+
     with open(file_path, "r") as file:
-        lines = file.readlines()
+        for line in file:
+            line = line.strip()
 
-    sequence = ""
-    for line in lines:
-        if not line.startswith(">"):
-            sequence += line.strip()
+            if line.startswith(">"):
+                if header:
+                    sequences[header] = seq
+                header = line[1:]
+                seq = ""
+            else:
+                seq += line
 
-    return sequence
+        if header:
+            sequences[header] = seq
 
-
-def gc_content(seq):
-    g = seq.count("G")
-    c = seq.count("C")
-    return (g + c) / len(seq) * 100
-
-
-def reverse_complement(seq):
-    complement = {"A":"T","T":"A","G":"C","C":"G"}
-    rev = ""
-
-    for base in seq[::-1]:
-        rev += complement.get(base, base)
-
-    return rev
-
-
-def dna_to_rna(seq):
-    return seq.replace("T","U")
-
+    return sequences
 
 file = "sample.fasta"
 
-sequence = read_fasta(file)
+sequences = read_fasta(file)
 
-print("Sequence:", sequence)
-print("Length:", len(sequence))
-print("GC Content:", gc_content(sequence))
-print("Reverse Complement:", reverse_complement(sequence))
-print("RNA:", dna_to_rna(sequence))
+for name, seq in sequences.items():
+    print("Sequence:", name)
+    print("Length:", len(seq))
+    print("GC Content:", gc_content(seq))
+    print("Reverse Complement:", reverse_complement(seq))
+    print("RNA:", dna_to_rna(seq))
+    print("----------------------")
